0a517e279dbaca7a211cafc8e6d9aaeab0a767ec
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-03-17 15:59:45 +0200

    removed marks

--- src/android/LocalNotification.java
+++ src/android/LocalNotification.java
@@ -563,7 +563,7 @@ public class LocalNotification extends CordovaPlugin {
             params = notification.toString() + "," + params;
 
             if(event.equals("click")){
-                String window_notification = "window['clicked_notification']= \"" + notification + "\"";
+                String window_notification = "window['clicked_notification']= " + notification;
 //                String window_notification2 = "\"window['clicked_notification']= test1\"";
 //                String window_notification = "window['clicked_notification'] = " + notification.toString();
                 eventQueue.add(window_notification);
